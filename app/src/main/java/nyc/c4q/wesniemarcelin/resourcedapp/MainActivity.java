365b5e6e5103f6e7b7ee06ccb6a2487178eb446d
HakeemSackes-Bramble <hakksaxx@outlook.com>
2017-01-29 11:17:40 -0500

    adding google maps

--- app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/MainActivity.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/MainActivity.java
@@ -1,13 +1,23 @@
 package nyc.c4q.wesniemarcelin.resourcedapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_hakeem);
+        mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map_frame_layout, mMapFragment);
+        fragmentTransaction.commit();
     }
 }
