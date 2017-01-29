bf8aefc1ed49f5ebb0eb886c59910123c1e23bd9
wesniem <wesniem@yahoo.com>
2017-01-29 14:59:52 -0500

    Update to Instructions Fragment

---
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/WelcomeFragment.java
@@ -0,0 +1,20 @@
+package nyc.c4q.wesniemarcelin.resourcedapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by wesniemarcelin on 1/29/17.
+ */
+
+public class WelcomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.welcome_layout, container, false);
+    }
+}
