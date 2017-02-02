6088d348bdcde3836027008198f2084be9ea21ef
wesniem <wesniem@yahoo.com>
2017-02-02 18:54:13 -0500

    Fixed conflicts

--- app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/backend/ChildCareClient.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/backend/ChildCareClient.java
@@ -21,7 +21,7 @@ public class ChildCareClient {
 
         }
     private static final String TAG = "Connection result";
-    private static String BASE_URL = "https://data.cityofnewyork.us/";git
+    private static String BASE_URL = "https://data.cityofnewyork.us/";
 
     public ArrayList<ArrayList<String>> getData() {
         return data;
