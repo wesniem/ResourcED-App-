73d802714cae3a243fbac59095c5533099d6546b
HakeemSackes-Bramble <hakksaxx@outlook.com>
2017-02-04 20:40:58 -0500

    last retrofit items

---
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/backend/places_gmaps/PlaceResponse.java
@@ -0,0 +1,9 @@
+package nyc.c4q.wesniemarcelin.resourcedapp.backend.places_gmaps;
+
+/**
+ * Created by hakeemsackes-bramble on 2/4/17.
+ */
+
+public class PlaceResponse {
+
+}
--- app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/google_map/MapsActivity_Hakeem.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/google_map/MapsActivity_Hakeem.java
@@ -1,8 +1,12 @@
 package nyc.c4q.wesniemarcelin.resourcedapp.google_map;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,11 +16,27 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 import nyc.c4q.wesniemarcelin.resourcedapp.R;
+import nyc.c4q.wesniemarcelin.resourcedapp.backend.ChildCareService;
+import nyc.c4q.wesniemarcelin.resourcedapp.backend.places_gmaps.PlaceResponse;
+import nyc.c4q.wesniemarcelin.resourcedapp.backend.places_gmaps.PlaceService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.R.attr.data;
 
 public class MapsActivity_Hakeem extends AppCompatActivity implements OnMapReadyCallback {
     private ArrayList<ArrayList<String>> retrofitData;
+    private ArrayList<ArrayList<String>> dataToImplement;
+    private EditText radius;
     private static final double ONEMILEINDEGREES = 1 / 69d;
     private GoogleMap mapDisplayed;
+    private EditText address;
+    int PLACE_PICKER_REQUEST = 1;
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +45,17 @@ public class MapsActivity_Hakeem extends AppCompatActivity implements OnMapReady
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        radius = (EditText) findViewById(R.id.radius);
+        address = (EditText) findViewById(R.id.search_bar_gmap);
+        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapDisplayed = googleMap;
+        mapMarker(mapDisplayed, dataToImplement);
     }
-
 
 
     void mapMarker(GoogleMap googleMap, ArrayList<ArrayList<String>> dataToMap) {
@@ -65,4 +88,30 @@ public class MapsActivity_Hakeem extends AppCompatActivity implements OnMapReady
     private double distanceFromPoint(LatLng a, LatLng b) {
         return Math.sqrt(Math.pow(a.longitude - b.longitude, 2) + Math.pow(a.latitude - b.latitude, 2));
     }
+
+    public void mapSearch(View v) {
+        dataToImplement = distanceSearch( new LatLng(0,0), Double.valueOf(radius.getText().toString());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+    private static void connectToServer(String baseUrl) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        PlaceService service = retrofit.create(PlaceService.class);
+        Call<PlaceResponse> call = service.getPlace();
+        call.enqueue(new Callback<PlaceResponse>() {
+            @Override
+            public void onResponse(Call<PlaceResponse> call, Response<PlaceResponse> response) {
+               // data = response.body();
+            }
+            @Override
+            public void onFailure(Call<PlaceResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
