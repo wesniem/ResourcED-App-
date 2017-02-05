3dccefe10cb7520d7fce89328b1c925eecb78373
wesniem <wesniem@yahoo.com>
2017-02-04 19:34:33 -0500

    Added SharedPreferences

--- app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/SpinnerActivity.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/resourcedapp/SpinnerActivity.java
@@ -1,6 +1,9 @@
 package nyc.c4q.wesniemarcelin.resourcedapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 
@@ -8,15 +11,5 @@ import android.widget.AdapterView;
  * Created by wesniemarcelin on 2/4/17.
  */
 
-public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-    }
+public class SpinnerActivity extends Activity {
 }
